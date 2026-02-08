78fe848c30506cbc465ee289e4a1e81864237c07
adamskirees <adamskirees@yahoo.com>
2026-02-09 08:06:24 +1000

    FIX: add event triggers and tidy repository

--- .test_package.py
+++
@@ -1,6 +0,0 @@
-import pytest
-from employee_events.employee import Employee
-
-def test_import():
-    """Simple test to ensure the package structure is correct."""
-    assert Employee is not None
